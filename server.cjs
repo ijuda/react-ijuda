f9a384e9a3e358f095e86c98e9a9042bc7564caf
mateuscesarglima <mateuscesarglima@gmail.com>
2022-08-16 21:35:09 -0300

    feat(IJUDA-1) deploy configuration

---
+++ server.cjs
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'dist')))
+app.set('port', process.env.PORT || 3000)
+
+const server = app.listen(app.get('port'), () => {
+    console.log("Listening on port", server.address().port)
+})
