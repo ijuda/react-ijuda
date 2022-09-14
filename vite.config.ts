c346d280af73c74c900533c9bcbf6fd3f4ed4d87
Mateus Cesar <mateuscesarglima@gmail.com>
2022-09-14 18:33:38 -0300

    feat(IJUDA-37) aliases configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: resolve(__dirname, "./src") },
+      {
+        find: "@containers",
+        replacement: resolve(__dirname, "./src/containers"),
+      },
+      {
+        find: "@components",
+        replacement: resolve(__dirname, "./src/components"),
+      },
+      {find: "@assets", replacement: resolve(__dirname, "./src/assets")}
+    ],
+  },
+});
