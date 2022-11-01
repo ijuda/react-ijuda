b7e87ac41dfaf5fa33b581eefc848d297bc444a9
Mateus Cesar <mateuscesarglima@gmail.com>
2022-11-01 08:46:48 -0300

    feat(IJUDA-50): update server.cjs.

--- server.cjs
+++ server.cjs
@@ -5,6 +5,13 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 3000);
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'), (err) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
 
 const server = app.listen(app.get('port'), () => {
   console.log('Listening on port', server.address().port);
