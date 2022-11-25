8ee956e3751a8789c66dfbc5e7b227bcb8c54080
Mateus Cesar <mateuscesarglima@gmail.com>
2022-11-25 19:23:39 -0300

    feat(IJUDA-54): improve api call.

--- src/api/services/getAllCategories.ts
+++ src/api/services/getAllCategories.ts
@@ -1,9 +1,5 @@
 import { api } from '@api/api';
 
 export const getAllCategories = () => {
-  const accessToken = localStorage.getItem('accessToken');
-
   return api.get('/categoria/categorias');
-
-  console.log('Error');
 };
